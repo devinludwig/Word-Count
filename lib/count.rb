20bf7b24b2a818931255fd4e5d74cdd2d78e5484
Devin Ludwig <devinludwig@gmail.com>
2016-11-18 08:58:05 -0800

    add method that passes first spec.

--- lib/count.rb
+++ lib/count.rb
@@ -1,5 +1,10 @@
 class String
-  define_method(:count) do
+  define_method(:count) do |search_word|
+    if self.==(search_word)
+      1
+    else
+      0
+    end
 
   end
 end
--- spec/count_spec.rb
+++ spec/count_spec.rb
@@ -1,9 +1,9 @@
 require('rspec')
-# require('./lib/method_here')
+require('./lib/count')
 
-# describe('Class#method_here') do
-  it('') do
-    expect('').to(eq(''))
+describe('String#count') do
+  it('returns 1 when the same word is entered as both the text and the search word') do
+    expect('one'.count('one')).to(eq(1))
   end
 
 end
