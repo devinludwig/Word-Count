64f14f794806a63ba9e2b97924f928222989fced
Devin Ludwig <devinludwig@gmail.com>
2016-11-18 11:20:59 -0800

    update highlight method to pass second spec by using regex to disregard case.

--- lib/highlight.rb
+++ lib/highlight.rb
@@ -1,6 +1,5 @@
 class String
   define_method(:highlight) do |search_word|
-    
-    gsub(search_word, '<strong>' + search_word +'</strong>')
+    gsub(/#{search_word}/i, '<strong>\0</strong>')
   end
 end
--- spec/highlight_spec.rb
+++ spec/highlight_spec.rb
@@ -3,6 +3,9 @@ require('./lib/highlight')
 
 describe('String#highlight') do
   it('returns input string with <strong> html tags around all instances of a search word argument') do
-    expect('"One lone ranger" hones the bone all alone! Will he telephone the crone? She is long gone.'.highlight('one')).to(eq('"One l<strong>one</strong> ranger" h<strong>one</strong>s the b<strong>one</strong> all al<strong>one</strong>! Will he teleph<strong>one</strong> the cr<strong>one</strong>? She is long g<strong>one</strong>.'))
+    expect('"one lone ranger" hones the bone all alone! Will he telephone the crone? She is long gone.'.highlight('one')).to(eq('"<strong>one</strong> l<strong>one</strong> ranger" h<strong>one</strong>s the b<strong>one</strong> all al<strong>one</strong>! Will he teleph<strong>one</strong> the cr<strong>one</strong>? She is long g<strong>one</strong>.'))
+  end
+  it('returns input string with <strong> html tags around all instances of a search word argument even in capitalized or uppercase form') do
+        expect('"One lone ranger" hones the bone all alone! Will he telephone the crone? She is LONG GONE.'.highlight('one')).to(eq('"<strong>One</strong> l<strong>one</strong> ranger" h<strong>one</strong>s the b<strong>one</strong> all al<strong>one</strong>! Will he teleph<strong>one</strong> the cr<strong>one</strong>? She is LONG G<strong>ONE</strong>.'))
   end
 end
